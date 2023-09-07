67f75269256a8c44dcd4e51284a38763696f91d4
LuizFrare <luizeduardo.frare@outlook.com>
2023-09-07 20:22:23 -0300

    feat: adjust animation

--- src/app/Components/BannerHero/Index.tsx
+++ src/app/Components/BannerHero/Index.tsx
@@ -6,8 +6,19 @@ export default function BannerHero() {
   return (
     <Wrapper>
       <Text>
-        <h1>Olá, eu sou o Luiz!</h1>
-        <h2>Desenvolvedor Front-end</h2>
+        <div className='content'>
+          <div className='content__container'>
+            <p className='content__container__text'>Hello</p>
+
+            <ul className='content__container__list'>
+              <li className='content__container__list__item'>world!</li>
+              <li className='content__container__list__item'>users!</li>
+              <li className='content__container__list__item'>friends!</li>
+              <li className='content__container__list__item'>everybody!</li>
+            </ul>
+          </div>
+        </div>
+        <h2>Eu sou um Desenvolvedor Front-end</h2>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
           voluptatum.
@@ -15,31 +26,32 @@ export default function BannerHero() {
       </Text>
       <ImageWrapper>
         <Image
+          className='Memoji'
           layout='responsive'
           width={400}
           height={400}
-          src='/IconBanner.png'
+          src='/MyMemoji.png'
           alt='teste'
         />
         <Image
           src='/Icons/cssIcon.svg'
           className='svgIcon css'
-          width={40}
-          height={40}
+          width={20}
+          height={20}
           alt='teste'
         />
         <Image
           src='/Icons/jsIcon.svg'
           className='svgIcon js'
-          width={40}
-          height={40}
+          width={20}
+          height={20}
           alt='teste'
         />
         <Image
           src='/Icons/reactIcon.svg'
           className='svgIcon react'
-          width={40}
-          height={40}
+          width={20}
+          height={20}
           alt='teste'
         />
       </ImageWrapper>
--- src/app/Components/BannerHero/styles.ts
+++ src/app/Components/BannerHero/styles.ts
@@ -5,17 +5,23 @@ export const Wrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 3rem 8rem;
+  padding: 3rem 6rem;
+  width: 100%;
+  max-width: 80rem;
+  margin: 0 auto;
 
-  img {
-    width: 100%;
+  .Memoji {
     max-width: 25rem;
     height: auto;
   }
 
-  @media (max-width: 768px) {
+  @media (max-width: 1024px) {
     flex-direction: column;
     padding: 2rem 4rem;
+
+    .Memoji {
+      width: 20rem !important;
+    }
   }
 `;
 
@@ -24,6 +30,197 @@ export const Text = styled.div`
   flex-direction: column;
   gap: 1rem;
   margin-right: 2rem;
+
+  .content {
+    position: relative;
+    overflow: hidden;
+    font-size: 2.1875rem;
+    line-height: 2.5rem;
+    color: #ecf0f1;
+
+    &__container {
+      font-weight: 600;
+      overflow: hidden;
+      height: 2.5rem;
+
+      &__text {
+        display: inline;
+        float: left;
+        margin: 0;
+      }
+
+      &__list {
+        margin-top: 0;
+        padding-left: 5.875rem;
+        text-align: left;
+        list-style: none;
+        animation-name: change;
+        animation-duration: 17s;
+        animation-iteration-count: infinite;
+
+        &__item {
+          line-height: 2.5rem;
+          margin: 0;
+        }
+      }
+    }
+  }
+
+  @-webkit-keyframes opacity {
+    0%,
+    100% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+  }
+
+  @-webkit-keyframes change {
+    0%,
+    12.66%,
+    100% {
+      transform: translate3d(0, 0, 0);
+    }
+    16.66%,
+    29.32% {
+      transform: translate3d(0, -25%, 0);
+    }
+    33.32%,
+    45.98% {
+      transform: translate3d(0, -50%, 0);
+    }
+    49.98%,
+    62.64% {
+      transform: translate3d(0, -75%, 0);
+    }
+    66.64%,
+    79.3% {
+      transform: translate3d(0, -50%, 0);
+    }
+    83.3%,
+    95.96% {
+      transform: translate3d(0, -25%, 0);
+    }
+  }
+
+  @-o-keyframes opacity {
+    0%,
+    100% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+  }
+
+  @-o-keyframes change {
+    0%,
+    12.66%,
+    100% {
+      transform: translate3d(0, 0, 0);
+    }
+    16.66%,
+    29.32% {
+      transform: translate3d(0, -25%, 0);
+    }
+    33.32%,
+    45.98% {
+      transform: translate3d(0, -50%, 0);
+    }
+    49.98%,
+    62.64% {
+      transform: translate3d(0, -75%, 0);
+    }
+    66.64%,
+    79.3% {
+      transform: translate3d(0, -50%, 0);
+    }
+    83.3%,
+    95.96% {
+      transform: translate3d(0, -25%, 0);
+    }
+  }
+
+  @-moz-keyframes opacity {
+    0%,
+    100% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+  }
+
+  @-moz-keyframes change {
+    0%,
+    12.66%,
+    100% {
+      transform: translate3d(0, 0, 0);
+    }
+    16.66%,
+    29.32% {
+      transform: translate3d(0, -25%, 0);
+    }
+    33.32%,
+    45.98% {
+      transform: translate3d(0, -50%, 0);
+    }
+    49.98%,
+    62.64% {
+      transform: translate3d(0, -75%, 0);
+    }
+    66.64%,
+    79.3% {
+      transform: translate3d(0, -50%, 0);
+    }
+    83.3%,
+    95.96% {
+      transform: translate3d(0, -25%, 0);
+    }
+  }
+
+  @keyframes opacity {
+    0%,
+    100% {
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+  }
+
+  @keyframes change {
+    0%,
+    12.66%,
+    100% {
+      transform: translate3d(0, 0, 0);
+    }
+    16.66%,
+    29.32% {
+      transform: translate3d(0, -25%, 0);
+    }
+    33.32%,
+    45.98% {
+      transform: translate3d(0, -50%, 0);
+    }
+    49.98%,
+    62.64% {
+      transform: translate3d(0, -75%, 0);
+    }
+    66.64%,
+    79.3% {
+      transform: translate3d(0, -50%, 0);
+    }
+    83.3%,
+    95.96% {
+      transform: translate3d(0, -25%, 0);
+    }
+  }
+
+  @media (max-width: 1024px) {
+    margin: 0 0 2rem 0;
+  }
 `;
 
 export const ImageWrapper = styled.div`
@@ -33,7 +230,7 @@ export const ImageWrapper = styled.div`
   margin-right: 2rem;
   position: relative;
   transform: translatey(0px);
-  animation: float 6s ease-in-out infinite;
+  animation: float 7s ease-in-out infinite;
 
   .svgIcon {
     width: 100%;
@@ -46,16 +243,19 @@ export const ImageWrapper = styled.div`
     &.react {
       top: 0;
       left: 2.5rem;
+      animation: floatReact 5s ease-in-out infinite;
     }
 
     &.js {
       bottom: 2.5rem;
       right: 0rem;
+      animation: floatJs 5s ease-in-out infinite;
     }
 
     &.css {
       bottom: 0;
       left: 2.5rem;
+      animation: floatCss 5s ease-in-out infinite;
     }
 
     @keyframes float {
@@ -63,13 +263,58 @@ export const ImageWrapper = styled.div`
         transform: translatey(0px) translateX(0px) rotate(0deg);
       }
       40% {
-        transform: translatey(-20px) translateX(-15px) rotate(1deg);
+        transform: translatey(-3px) translateX(-4px) rotate(0.3deg);
       }
       80% {
-        transform: translatey(-5px) translateX(-5px) rotate(-1deg);
+        transform: translatey(-2px) translateX(-3px) rotate(0.1deg);
       }
       100% {
         transform: translatey(0px) translateX(0px) rotate(0deg);
+      }
+    }
+
+    @keyframes floatJs {
+      0% {
+        transform: translateX(0px) rotate(0deg);
+      }
+      40% {
+        transform: translateX(-0.2px) rotate(-5deg);
+      }
+      80% {
+        transform: translateX(-0.1px) rotate(-0.2deg);
+      }
+      100% {
+        transform: translateX(0px) rotate(0deg);
+      }
+    }
+
+    @keyframes floatReact {
+      0% {
+        transform: translateX(0px) rotate(0deg);
+      }
+      40% {
+        transform: translateX(-8px) rotate(-4deg);
+      }
+      80% {
+        transform: translateX(-0.1px) rotate(-0.2deg);
+      }
+      100% {
+        transform: translateX(0px) rotate(0deg);
+      }
+    }
+
+    @keyframes floatCss {
+      0% {
+        transform: translateX(0px) rotate(0deg);
+      }
+      40% {
+        transform: translateX(4px) rotate(9deg);
+      }
+      80% {
+        transform: translateX(-0.1px) rotate(-2deg);
+      }
+      100% {
+        transform: translateX(0px) rotate(0deg);
       }
     }
   }
